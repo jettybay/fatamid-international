23adcd8c60060acf275ee1318229be64c752412f
jettybay <ojetb1@yahoo.com>
2026-03-27 21:14:50 +0100

    Sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://fatamid.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://fatamid.com/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://fatamid.com/services",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://fatamid.com/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
