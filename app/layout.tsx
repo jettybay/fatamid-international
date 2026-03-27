dbb6c32c476dc901c627298c6d32720376ef7fc2
jettybay <ojetb1@yahoo.com>
2026-03-27 21:41:36 +0100

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,16 @@ export const metadata: Metadata = {
   verification: {
     google: "SCD9WgMb_6kbwIkjqI950-OvMJIJTgsp8M7TX24uJog",
   },
+  icons: {
+    icon: [
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
